1590375c39c83f0024f40d213225d8eeae6029d3
संकेत <indahud@protonmail.com>
2023-11-22 16:07:23 +0530

    feat: new images added

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,13 +2,13 @@ const projectsData = [
   {
     title: 'Mockingjay',
     description: `Mock API to create UI without any backend.`,
-    imgSrc: 'https://picsum.photos/seed/picsum/800/400',
+    imgSrc: 'https://raw.githubusercontent.com/indahud/sanketh.me/main/public/static/images/api.webp',
     href: '/blog/mockingjay',
   },
   {
     title: 'Upilink',
     description: `Recieve payment easily with getUpiLink`,
-    imgSrc: 'https://picsum.photos/seed/picsum/800/400',
+    imgSrc: 'https://raw.githubusercontent.com/indahud/sanketh.me/main/public/static/images/payment.webp',
     href: '/blog/upilink',
   },
 ]
