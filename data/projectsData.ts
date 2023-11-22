9150f955da63ce1a4b06601d2daf926d8adfb12f
संकेत <indahud@protonmail.com>
2023-11-22 15:15:55 +0530

    init

---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Mockingjay',
+    description: `Mock API to create UI without any backend.`,
+    imgSrc: 'https://picsum.photos/seed/picsum/800/400',
+    href: '/blog/mockingjay',
+  },
+  {
+    title: 'Upilink',
+    description: `Recieve payment easily with getUpiLink`,
+    imgSrc: 'https://picsum.photos/seed/picsum/800/400',
+    href: '/blog/upilink',
+  },
+]
+
+export default projectsData
