59d325385a82e9ed18eb5f6f02b36903c5ca1741
indahud <thesanketshete@gmail.com>
2024-04-09 16:24:12 +0530

    feat: New project added

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,6 +11,12 @@ const projectsData = [
     imgSrc: 'https://raw.githubusercontent.com/indahud/sanketh.me/main/public/static/images/payment.webp',
     href: '/blog/upilink',
   },
+  {
+    title: 'getupilink Utility Tools',
+    description: `Different calculators and utilities for productivity.`,
+    imgSrc: 'https://raw.githubusercontent.com/indahud/sanketh.me/main/public/static/images/06_tools_getupilink.webp',
+    href: '/blog/utility-tools',
+  },
 ]
 
 export default projectsData
