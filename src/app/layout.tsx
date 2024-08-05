7a4dffb58f8c396c7f9beefc21758ab6d64449f9
indahud <thesanketshete@gmail.com>
2024-08-05 18:45:03 +0530

    feat: revamp

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from 'next';
+
+import { Providers } from '@/app/providers';
+import { Layout } from '@/components/Layout';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Sanketh',
+    default:
+      'Sanketh - Software Developer, Indie Hacker',
+  },
+  description:
+    'I’m Sanket, a software developer based in PUne, India.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/lib/common.ts
@@ -0,0 +1,33 @@
+export const CLOUDFLARE_BUCKET_PUBLIC =
+  'https://pub-89e353e5e5954066b8a84a8a037ec4a1.r2.dev';
+
+const Projects = [
+  {
+    title: 'App link Opener',
+    description: '',
+    date: '',
+  },
+];
+
+export const HOMEPAGE_IMGS = [
+  {
+    name: 'Cockpit',
+    src: `${CLOUDFLARE_BUCKET_PUBLIC}/image1.webp`,
+  },
+  {
+    name: 'Nix',
+    src: `${CLOUDFLARE_BUCKET_PUBLIC}/image2.webp`,
+  },
+  {
+    name: 'Do-more',
+    src: `${CLOUDFLARE_BUCKET_PUBLIC}/image3.webp`,
+  },
+  {
+    name: 'Banksy',
+    src: `${CLOUDFLARE_BUCKET_PUBLIC}/image4.webp`,
+  },
+  {
+    name: 'Alps',
+    src: `${CLOUDFLARE_BUCKET_PUBLIC}/image5.webp`,
+  },
+];
